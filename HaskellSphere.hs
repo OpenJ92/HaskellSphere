6004a5976f8207d33398571c01d5e85d5ba59f1b
OpenJ92 <jacob.vartuli.92@gmail.com>
2020-04-06 13:39:30 -0400

    Genterate NDArrays from lists

--- HaskellSphere.hs
+++ HaskellSphere.hs
@@ -40,6 +40,19 @@ getValue _       (Value x) = x
 getValue (i:idx) (Array x) = getValue idx (x!!i)
 
 -- This can be reconstructed as a mutually recursive set of functions. 
+-- It might be nessesary to construct a recursive list construction so 
+-- as to partition the input as we traverse the tree.
+
+fill' :: [a] -> [Int] ->  NDArray a
+fill' ns [x]    = Array $ loop' ns ((-) x 1)
+fill' ns (x:xs) = Array $ loop  ns (length ns)  ((-) x 1) x xs
+
+loop' []     _  = []
+loop' (n:ns) x' = Value n : loop' ns ((-) x' 1)
+
+loop ns _   0  _ xs = [ fill' ns xs ]
+loop ns lns x' x xs = fill' (take (div lns x) ns) xs : loop (drop (div lns x) ns) lns ((-) x' 1) x xs
+
 fill :: a -> [Int] ->  NDArray a
 fill n [x]    = Array [ Value n   | _ <- [1..x]]
 fill n (x:xs) = Array [ fill n xs | _ <- [1..x] ]
